26970046ca9032442e8cee8f76edfcc42b3f28f7
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-20 16:27:49 -0300

    Clase 4: Configuración del archivo jsconfig.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import "/home/alvaro/Escritorio/Proyectos/curso-next12/src/styles/tailwind.css";
+import "@styles/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
