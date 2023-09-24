526001011e8dec9ed12cc51edebae4353a601535
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-24 05:17:06 -0300

    Clase 11: Autenticándonos en la API usando Axios

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,12 @@
 import React, { useState, useContext, createContext } from "react";
 import Cookie from "js-cookie";
 import axios from "axios";
+import endPoints from "@services/api/";
 
 const AuthContext = createContext();
 
 export function ProviderAuth({ children }) {
-  const auth = useProviderAuth();
+  const auth = useProvideAuth();
   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 }
 
@@ -13,11 +14,18 @@ export const useAuth = () => {
   return useContext(AuthContext);
 };
 
-function useProviderAuth() {
+function useProvideAuth() {
   const [user, setUser] = useState(null);
 
   const signIn = async (email, password) => {
-    setUser("login");
+    const options = {
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+      },
+    };
+    const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
+    console.log(access_token);
   };
 
   return {
