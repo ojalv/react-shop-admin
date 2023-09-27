f8a3bc0513335672e0f1809cd58b40bb65fda09f
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-26 22:50:02 -0300

    Clase 26: Implementación del logout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -33,9 +33,16 @@ function useProvideAuth() {
       setUser(user);
     }
   };
+  const logout = () => {
+    Cookie.remove("token");
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = "/login";
+  };
 
   return {
     user,
     signIn,
+    logout,
   };
 }
