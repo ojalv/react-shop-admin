847b66d072490a47903a5034c17db658b3197c92
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-26 22:18:07 -0300

    Clase 23: Eliminado de productos en la API

--- src/pages/dashboard/products.js
+++ src/pages/dashboard/products.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
-import { CheckIcon } from "@heroicons/react/24/solid";
+import { CheckIcon, XCircleIcon } from "@heroicons/react/24/solid";
 import Modal from "@common/Modal";
 import FormProduct from "@components/FormProduct";
 import axios from "axios";
 import endPoints from "@services/api";
 import useAlert from "@hooks/useAlert";
 import Alert from "@common/Alert";
-
+import { deleteProduct } from "@services/api/products";
 export default function Products() {
   const [open, setOpen] = useState(false);
   const [products, setProducts] = useState([]);
@@ -23,6 +23,17 @@ export default function Products() {
       console.log(error);
     }
   }, [alert]);
+
+  const handleDelete = (id) => {
+    deleteProduct(id).then(() => {
+      setAlert({
+        active: true,
+        message: "Delete product successfully",
+        type: "error",
+        autoClose: true,
+      });
+    });
+  };
   return (
     <>
       <Alert alert={alert} handleClose={toggleAlert} />
@@ -96,9 +107,7 @@ export default function Products() {
                         </a>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="/edit" className="text-indigo-600 hover:text-indigo-900">
-                          Delete
-                        </a>
+                        <XCircleIcon className="flex-shrink-0 h-6 w-6 text-gray-400 cursor-pointer" aria-hidden="true" onClick={() => handleDelete(product.id)} />
                       </td>
                     </tr>
                   ))}
